40f8ed6e4c6f9da29608347992ace63b30c40506
DBaird-EOU <dbaird@eou.edu>
2015-05-05 16:10:47 -0700

    Added gradle files necessary to build the project.

---
+++ desktop/src/releasethekraken/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package releasethekraken.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import releasethekraken.ReleaseTheKraken;
+
+public class DesktopLauncher
+{
+
+    public static void main(String[] arg)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new ReleaseTheKraken(), config);
+    }
+}
