89422769e5b22fd01b14ad56621788f92539c227
Lucas Schuetz <lucas.schuetz.23@gmail.com>
2015-05-10 19:48:43 -0700

    Added from Entity.java: 	-Two constuctors 	-GameWorld instance variable 	-float X Velocity 	-float Y Velocity
    Changed from Entity.java:
    	-Made locations floats
    
    Removed from Entity.java:
    	-Sprite instance variable

--- core/src/releasethekraken/entity/Entity.java
+++ core/src/releasethekraken/entity/Entity.java
@@ -5,10 +5,11 @@
  */
 package releasethekraken.entity;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.utils.Disposable;
+import releasethekraken.GameWorld;
 import releasethekraken.ui.Renderable;
 /**
  * The skeleton class for all entities.
@@ -27,12 +28,26 @@ import releasethekraken.ui.Renderable;
  *  -dispose() - overriden from the Disposable class
  * 
  * --5/10/15--
- * -removed methods
+ * -added variables
+ *  -float xVel - X Velocity
+ *  -float yVel - Y Velocity
+ *  -GameWorld world - where the entity exists
+ * 
+ * -changed variables
+ *  -int xLoc --> float xLoc
+ *  -int yLoc --> float yLoc
+ * 
+ * -removed
  *  -render() - removed and replaced by the following added methods
+ *  -Sprite sprite - sprite will be stored in GameAssets file
  * 
  * -added methods
  *  -renderShapes(ShapeRenderer)
  *  -renderSprite(SpriteBatch)
+ *  -default constructor
+ *      -takes GameWorld and the x/y coordinates as arguments
+ *  -constructor
+ *      -takes GameWorld and RectangleMapObject as arguments
  * 
  * -implemented Renderable
  * 
@@ -40,9 +55,35 @@ import releasethekraken.ui.Renderable;
  */
 public class Entity implements Disposable, Renderable
 {
-    protected int xLoc;
-    protected int yLoc;
-    protected Sprite sprite;
+    protected GameWorld world;
+    
+    protected float xLoc;
+    protected float yLoc;
+    protected float xVel;
+    protected float yVel;
+    
+    /**
+     *  Default constructor
+     * 
+     * @param world The GameWorld the entity will be spawned in
+     * @param xLocation The initial x coordinate
+     * @param yLocation The initial y coordinate
+     */
+    public Entity(GameWorld world, float xLocation, float yLocation)
+    {
+        
+    }
+    
+    /**
+     * Constructor that takes the game world and a RectangleMapObject
+     * 
+     * @param world
+     * @param mapObject
+     */
+    public Entity(GameWorld world, RectangleMapObject mapObject)
+    {
+        
+    }
     
     @Override
     public void renderShapes(ShapeRenderer shapeRenderer)
