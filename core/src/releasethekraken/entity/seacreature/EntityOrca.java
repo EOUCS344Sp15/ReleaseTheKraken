92ec39b565eefbab64270b75ccbf1022a891837e
sbartlett <sbartlett@cslab18.cs.eou.edu>
2015-05-21 16:06:24 -0700

    Added EntityOrca class

---
+++ core/src/releasethekraken/entity/seacreature/EntityOrca.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package releasethekraken.entity.seacreature;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import releasethekraken.GameAssets;
+import releasethekraken.GameWorld;
+import releasethekraken.entity.projectile.EntityProjectile;
+import releasethekraken.entity.projectile.EntityWaterSquirt;
+
+/**
+ *
+ * @author sbartlett
+ */
+public class EntityOrca extends EntitySeaCreature
+{    
+    //Primary constructor
+    public EntityOrca(GameWorld world, float xLocation, float yLocation)
+    {
+        super(world, xLocation, yLocation);
+        
+        //TODO: Change these
+        this.health = 10;
+        this.maxHealth = 10;
+    }
+    
+    //Secondary constructor
+    public EntityOrca(GameWorld world, TextureMapObject mapObject)
+    {
+        super(world, mapObject);
+        //This will be implemented when the level loader is written
+        
+        //TODO: Change these
+        this.health = 10;
+        this.maxHealth = 10;
+    }
+    
+    @Override
+    public void renderShapes(ShapeRenderer shapeRenderer)
+    {
+        super.renderShapes(shapeRenderer);
+    }
+    
+    @Override
+    public void renderSprites(SpriteBatch batch)
+    {
+        super.renderSprites(batch);
+        
+        float spriteUnitWidth = 2F;
+        batch.draw(GameAssets.entityFishTexture, //TODO: Need Orca Sprite
+                this.pos.x - spriteUnitWidth/2,
+                this.pos.y - spriteUnitWidth/2,
+                spriteUnitWidth,
+                spriteUnitWidth);
+    }
+    
+    /**
+     * Builds and adds a new projectile to the world
+     */
+    public void attack()
+    {
+        //TODO: Make EntityWaterBomb for the Orca attack
+        this.world.addEntity(new EntityWaterSquirt(world, this.pos.x, this.pos.y, this.vel.x, this.vel.y, this));
+    }
+}
